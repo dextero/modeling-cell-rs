f3e3f39d4640a01843fad53f6b1462ae0b1d2997
Marcin <marcin@mradomski.pl>
2016-01-07 11:24:26 +0100

    Add command-line options

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate rand;
+extern crate getopts;
 
 extern crate piston;
 extern crate graphics;
 extern crate glutin_window;
 extern crate opengl_graphics;
+
+use std::fmt;
 
 use piston::window::WindowSettings;
 use piston::event_loop::*;
@@ -69,7 +72,95 @@ impl App {
     }
 }
 
+struct Options {
+    board_size: (usize, usize)
+}
+
+enum ParseResult {
+    Success(Options),
+    Failure(String),
+    Exit
+}
+
+impl Options {
+    fn parse_csv_ints(string: &String) -> Result<Vec<usize>, String> {
+        let mut sizes = Vec::new();
+        for substr in string.split(",") {
+            sizes.push(match substr.parse::<usize>() {
+                Ok(v) => v,
+                Err(e) => return Err(e.to_string())
+            })
+        }
+
+        Ok(sizes)
+    }
+
+    fn parse_size(string: Option<String>) -> Result<(usize,usize), String> {
+        match string {
+            None => Ok((80, 60)),
+            Some(s) => {
+                let sizes = try!(Options::parse_csv_ints(&s));
+
+                match sizes.len() {
+                    1 => Ok((sizes[0], sizes[0])),
+                    2 => Ok((sizes[0], sizes[1])),
+                    _ => {
+                        let msg = format!("invalid argument format: {}, expected SIZE or WIDTH,HEIGHT",
+                                          s);
+                        Err(msg)
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn from_cmdline() -> ParseResult {
+        let args: Vec<String> = std::env::args().collect();
+
+        let mut opts = getopts::Options::new();
+        opts.optopt("s", "board-size", "set board size", "WIDTH,HEIGHT");
+        opts.optflag("h", "help", "print this help message");
+
+        let matches = match opts.parse(&args[1..]) {
+            Ok(matches) => matches,
+            Err(e) => return ParseResult::Failure(e.to_string())
+        };
+
+        if matches.opt_present("h") {
+            let msg = format!("Usage: {} [options]", args[0]);
+            print!("{}", opts.usage(&msg));
+            return ParseResult::Exit
+        }
+
+        let board_size = match Options::parse_size(matches.opt_str("s")) {
+            Ok(size) => size,
+            Err(e) => return ParseResult::Failure(e)
+        };
+
+        ParseResult::Success(Options {
+                board_size: board_size
+        })
+    }
+}
+
+impl fmt::Display for Options {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "board_size: {}, {}", self.board_size.0, self.board_size.1)
+    }
+}
+
 fn main() {
+    let opts = match Options::from_cmdline() {
+        ParseResult::Success(opts) => opts,
+        ParseResult::Failure(reason) => {
+            println!("{}", reason);
+            return
+        },
+        ParseResult::Exit => return
+    };
+
+    println!("Configuration:\n{}", opts);
+
     let gl_version = OpenGL::V3_2;
 
     let window: Window = WindowSettings::new("cell", [800, 600])
@@ -80,7 +171,8 @@ fn main() {
 
     let mut app = App {
         gl: GlGraphics::new(gl_version),
-        board: Board::new_random(80, 60),
+        board: Board::new_random(opts.board_size.0,
+                                 opts.board_size.1),
         time_accumulator: TimeAccumulator::new(0.01f64)
     };
 
